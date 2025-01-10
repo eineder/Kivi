d09b334aa028e88a5a69403fd7c6f51b1b30226b
eineder <49588296+eineder@users.noreply.github.com>
2025-01-10 10:18:41 +0100

    Added tests

--- api/api.go
+++ api/api.go
@@ -1,12 +1,20 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"strings"
 
 	"github.com/eineder/kivi/store"
 )
+
+type Response struct {
+	Message string      `json:"message"`
+	Success bool        `json:"success"`
+	Payload interface{} `json:"payload"`
+}
 
 var s store.Store = store.NewInMemoryStore()
 
@@ -43,47 +51,63 @@ func itemsHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func createItem(w http.ResponseWriter, r *http.Request, key string) {
-	value := r.FormValue("value")
-	err := s.CreateItem(key, value)
+	value, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+	err = s.CreateItem(key, string(value))
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusCreated)
-	w.Write([]byte(fmt.Sprintf(`{"message": "Item %s created"}`, key)))
+	writeResponse(w, true, fmt.Sprintf("Item %s created", key), nil)
 }
 
 func getItem(w http.ResponseWriter, r *http.Request, key string) {
 	value, err := s.GetItem(key)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`{"item": {"key": "%s", "value": "%s"}}`, key, value)))
+	if err != nil {
+		writeResponse(w, false, err.Error(), nil)
+		return
+	}
+	writeResponse(w, true, "Value for key found", value)
 }
 
 func updateItem(w http.ResponseWriter, r *http.Request, key string) {
-	value := r.FormValue("value")
-	err := s.UpdateItem(key, value)
+	value, err := io.ReadAll(r.Body)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
 		return
 	}
-	w.Header().Set("Content-Type", "application/json")
+
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`{"message": "Item %s updated"}`, key)))
+	w.Header().Set("Content-Type", "application/json")
+	err = s.UpdateItem(key, string(value))
+
+	if err != nil {
+		writeResponse(w, false, fmt.Sprintf("Item %s not found", key), nil)
+		return
+	}
+
+	writeResponse(w, true, fmt.Sprintf("Item %s updated", key), nil)
 }
 
 func deleteItem(w http.ResponseWriter, r *http.Request, key string) {
-	err := s.DeleteItem(key)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusNotFound)
-		return
-	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(fmt.Sprintf(`{"message": "Item %s deleted"}`, key)))
+	err := s.DeleteItem(key)
+
+	if err != nil {
+		writeResponse(w, false, fmt.Sprintf("Item %s not found", key), nil)
+		return
+	}
+	writeResponse(w, true, fmt.Sprintf("Item %s deleted", key), nil)
+}
+
+func writeResponse(w http.ResponseWriter, success bool, message string, payload interface{}) {
+	json.NewEncoder(w).Encode(Response{Success: success, Message: message, Payload: payload})
 }
--- store/inmemorystore.go
+++ store/inmemorystore.go
@@ -1,6 +1,9 @@
 package store
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type InMemoryStore struct {
 	data map[string]string
@@ -17,6 +20,7 @@ func (s *InMemoryStore) CreateItem(key string, value string) error {
 		return errors.New("item already exists")
 	}
 	s.data[key] = value
+	fmt.Printf("\nCreated item '%s' with value '%s'", key, value)
 	return nil
 }
 
@@ -25,6 +29,7 @@ func (s *InMemoryStore) GetItem(key string) (string, error) {
 	if !exists {
 		return "", errors.New("item not found")
 	}
+	fmt.Printf("\nGot item '%s' with value '%s'", key, value)
 	return value, nil
 }
 
@@ -33,6 +38,7 @@ func (s *InMemoryStore) UpdateItem(key string, value string) error {
 		return errors.New("item not found")
 	}
 	s.data[key] = value
+	fmt.Printf("\nUpdated item '%s' with value '%s'", key, value)
 	return nil
 }
 
@@ -41,5 +47,6 @@ func (s *InMemoryStore) DeleteItem(key string) error {
 		return errors.New("item not found")
 	}
 	delete(s.data, key)
+	fmt.Printf("\nDeleted item '%s'", key)
 	return nil
 }
